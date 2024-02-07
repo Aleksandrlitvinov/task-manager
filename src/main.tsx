170a692d9e4ba742a2e60282f9cd9652ab632724
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-02-07 10:12:29 +0100

    feat: remove strictMode, add Card component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-import { StrictMode } from 'react'
-
 import { createRoot } from 'react-dom/client'
 
+import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
-import './styles/index.scss'
 
 import { App } from './App'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+createRoot(document.getElementById('root') as HTMLElement).render(<App />)
