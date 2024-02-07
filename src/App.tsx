243b65b8e18c69bc636df9426404fb484899d19d
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-02-07 14:16:17 +0100

    ref: remove Button from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import { Button } from './components'
-
 export const App = () => {
-  return (
-    <div style={{ marginLeft: '10px' }}>
-      <Button variant={'success'}>Hello</Button>
-    </div>
-  )
+  return <div>Hello</div>
 }
