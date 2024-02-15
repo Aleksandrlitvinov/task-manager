070a57a18cc9cb72b26651e37bb8224ea9a678d3
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-02-15 22:31:18 +0100

    feat: add sign-in react-hook-form with zod validation

---
+++ src/widgets/ui/sign-in-form/services/login-schema.ts
@@ -0,0 +1,13 @@
+import * as z from 'zod'
+
+import { PASSWORD_PATTERN } from './regex'
+
+export const loginSchema = () => {
+  return z.object({
+    email: z.string().email({ message: 'incorrect message' }).trim(),
+    password: z.string().min(6).max(20).regex(PASSWORD_PATTERN).trim(),
+    rememberMe: z.boolean(),
+  })
+}
+
+export type loginFormValuesType = z.infer<ReturnType<typeof loginSchema>>
---
+++ src/widgets/ui/sign-in-form/ui/sign-in-form.tsx
@@ -0,0 +1,83 @@
+import { Controller, FormProvider, useForm } from 'react-hook-form'
+
+import { Input, stylesAddItemForm, stylesBtnTask } from '@/components'
+import { loginFormValuesType, loginSchema } from '@/widgets'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Checkbox, FormControlLabel, FormGroup, ThemeProvider } from '@mui/material'
+
+import s from './sign-in-form.module.scss'
+
+export const SignInForm = () => {
+  const methods = useForm<loginFormValuesType>({
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+    resolver: zodResolver(loginSchema()),
+  })
+
+  const onHandleSubmit = (data: loginFormValuesType) => console.log(data)
+
+  return (
+    <FormProvider {...methods}>
+      <form className={s.form} onSubmit={methods.handleSubmit(onHandleSubmit)}>
+        <FormGroup defaultValue={''}>
+          <div className={s.textField}>
+            <ThemeProvider theme={stylesAddItemForm}>
+              <Controller
+                control={methods.control}
+                name={'email'}
+                render={({ field, fieldState: { error } }) => (
+                  <Input
+                    errorInput={error?.message}
+                    label={'Email'}
+                    placeholder={'Write your email'}
+                    type={'text'}
+                    {...field}
+                  />
+                )}
+              />
+            </ThemeProvider>
+          </div>
+          <div className={s.textField}>
+            <ThemeProvider theme={stylesAddItemForm}>
+              <Controller
+                control={methods.control}
+                name={'password'}
+                render={({ field, fieldState: { error } }) => (
+                  <Input
+                    errorInput={error?.message}
+                    label={'Password'}
+                    placeholder={'Write your password'}
+                    type={'text'}
+                    {...field}
+                  />
+                )}
+              />
+            </ThemeProvider>
+          </div>
+          <div className={s.formCheckbox}>
+            <Controller
+              control={methods.control}
+              name={'rememberMe'}
+              render={({ field }) => (
+                <FormControlLabel
+                  control={<Checkbox required={false} {...field} />}
+                  label={'Remember Me'}
+                />
+              )}
+            />
+          </div>
+        </FormGroup>
+        <div className={s.formBtn}>
+          <ThemeProvider theme={stylesBtnTask}>
+            <Button fullWidth type={'submit'} variant={'contained'}>
+              Login
+            </Button>
+          </ThemeProvider>
+        </div>
+      </form>
+    </FormProvider>
+  )
+}
