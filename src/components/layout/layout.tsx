c817cd342acacce421d6475fb700ba371cbd1587
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-02-16 09:18:17 +0100

    ref: add grid styles for Container

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,25 +1,29 @@
 import { Outlet } from 'react-router-dom'
 
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, Container, Toolbar, Typography } from '@mui/material'
 
 import s from './layout.module.scss'
 
 export const Layout = () => {
   return (
     <>
-      <AppBar position={'static'}>
-        <Toolbar style={{ backgroundColor: 'var(--color-accent-700)' }}>
-          <Typography color={'inherit'} component={'div'} sx={{ flexGrow: 1 }} variant={'h5'}>
-            TASK MANAGER
-          </Typography>
-          <Button color={'inherit'} variant={'outlined'}>
-            Logout
-          </Button>
-        </Toolbar>
+      <AppBar position={'static'} style={{ backgroundColor: 'var(--color-accent-700)' }}>
+        <Container>
+          <Toolbar>
+            <Typography color={'inherit'} component={'div'} sx={{ flexGrow: 1 }} variant={'h5'}>
+              TASK MANAGER
+            </Typography>
+            <Button color={'inherit'} variant={'outlined'}>
+              Logout
+            </Button>
+          </Toolbar>
+        </Container>
       </AppBar>
-      <main className={s.content}>
-        <Outlet />
-      </main>
+      <Container>
+        <main className={s.content}>
+          <Outlet />
+        </main>
+      </Container>
     </>
   )
 }
